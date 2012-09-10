e68c0db1d1d15f250a65ba86c5882dabd93dd1aa
Andrew Danger Lyon <orthecreedence@gmail.com>
2012-09-09 23:35:37 -0700

    fixing arbiter crap wto work with new clipmunk binding updates. yegh

--- arbiter.lisp
+++ arbiter.lisp
@@ -4,17 +4,16 @@
   "Given an arbiter C pointer (ie one passed from a collision callback), grab
    the collision normal as a list '(x y)."
   (assert (and (<= 0 i)
-               (< (cp-a:arbiter-num-contacts arbiter) i)))
-  (let ((vect (cp-a:contact-n (cffi:mem-aref (cp-a:arbiter-contacts arbiter) 'cp:contact i))))
-    (list (cp-a:vect-x vect)
-          (cp-a:vect-y vect))))
+               (< i (cp-a:arbiter-num-contacts arbiter))))
+  (let ((contact (cffi:mem-aref (cp-a:arbiter-contacts arbiter) 'cp:contact i)))
+    (list (cp-a:contact-n-x contact)
+          (cp-a:contact-n-y contact))))
 
 (defun get-arbiter-point (arbiter i)
   "Given an arbiter C pointer (ie one passed from a collision callback), grab
    the collision point as a list '(x y)."
   (assert (and (<= 0 i)
-               (< (cp-a:arbiter-num-contacts arbiter) i)))
-  (let ((vect (cp-a:contact-p (cffi:mem-aref (cp-a:arbiter-contacts arbiter) 'cp:contact i))))
-    (list (cp-a:vect-x vect)
-          (cp-a:vect-y vect))))
-
+               (< i (cp-a:arbiter-num-contacts arbiter))))
+  (let ((contact (cffi:mem-aref (cp-a:arbiter-contacts arbiter) 'cp:contact i)))
+    (list (cp-a:contact-p-x contact)
+          (cp-a:contact-p-y contact))))
